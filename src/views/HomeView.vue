a301208655894cfca975d9551344f6e20bd865a1
Vincent Jacques <vincent@vincent-jacques.net>
2025-01-31 12:07:01 +0100

    Prototype displaying a graph with two movable nodes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,15 @@ import TwoNodes from '../components/TwoNodes.vue'
 </script>
 
 <template>
-  <TwoNodes />
+  <div style="display: flex; flex-direction: row; height: 100vh; width: 100%">
+    <div style="width: 1.5em; background-color: lightblue">L</div>
+    <div style="flex: 1; overflow: hidden; display: flex; flex-direction: column">
+      <div style="height: 1.5em; background-color: lightblue">T</div>
+      <div style="flex: 1; overflow: hidden">
+        <TwoNodes />
+      </div>
+      <div style="height: 1.5em; background-color: lightblue">B</div>
+    </div>
+    <div style="width: 1.5em; background-color: lightblue">R</div>
+  </div>
 </template>
