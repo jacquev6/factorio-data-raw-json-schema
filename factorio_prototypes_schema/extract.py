b9fa27cf76bfe5be2b61af560b1a0f5e91dd38c5
Vincent Jacques <vincent@vincent-jacques.net>
2025-02-11 19:35:47 +0100

    Move ad-hoc patches to their own file

--- factorio_prototypes_schema/extract.py
+++ factorio_prototypes_schema/extract.py
@@ -125,7 +125,7 @@ def main(factorio_location: str) -> None:
     types = list(extract_all_types(factorio_location))
     prototypes = list(extract_all_prototypes(factorio_location, {type.name for type in types}))
 
-    schema: Any = FactorioSchema(
+    schema: JsonValue = FactorioSchema(
         properties={
             # @todo Extract
             "ammo": "AmmoItemPrototype",
@@ -156,55 +156,6 @@ def main(factorio_location: str) -> None:
         types=types,
         prototypes=prototypes,
     ).to_json_value()
-
-    # Ad-hoc patches because the doc doesn't match the actual data
-    # ============================================================
-
-    # types/ItemProductPrototype.html#amount_min is documented as uint16
-    # but is some sort of floating point in e.g. 'speed-module-recycling'
-    if schema["definitions"].get("ItemProductPrototype", {}).get("properties", {}).get("amount", {}) == {
-        "$ref": "#/definitions/uint16"
-    }:
-        schema["definitions"]["ItemProductPrototype"]["properties"]["amount"] = {"$ref": "#/definitions/double"}
-    else:
-        debug("Failed to patch ItemProductPrototype")
-
-    # types/ItemStackIndex.html documents ItemStackIndex an alias for uint16
-    # but it's set to "dynamic" in blueprint book
-    if schema["definitions"].get("ItemStackIndex", {}).get("anyOf", []) == [{"$ref": "#/definitions/uint16"}]:
-        schema["definitions"]["ItemStackIndex"]["anyOf"].append({"type": "string", "const": "dynamic"})
-    else:
-        debug("Failed to patch ItemStackIndex")
-
-    # types/TriggerEffect.html documents DamageTileTriggerEffectItem as having type="damage-tile",
-    # but DamageTileTriggerEffectItem.html documents it as having type="damage"
-
-    # types/TriggerEffect.html refers to non-documented type DamageEntityTriggerEffectItem,
-    # and types/DamageTriggerEffectItem.html is documented but used nowhere
-
-    # Empty arrays are serialized as {} instead of []
-    # (patched in the generated schema because this must be done everywhere)
-
-    # I'm fed up with documenting every single patch
-    schema["definitions"]["WorkingVisualisations"]["properties"]["working_visualisations"] = {}
-    schema["definitions"]["CharacterPrototype"]["allOf"][1]["properties"]["synced_footstep_particle_triggers"] = {}
-
-    # Ad-hoc patches because our extraction tool is weak
-    # ==================================================
-
-    # RandomRange can be {min: double, max: double}
-    if schema["definitions"].get("RandomRange", {}).get("anyOf", [{}])[0] == {"$ref": "#/definitions/{"}:
-        schema["definitions"]["RandomRange"]["anyOf"] = schema["definitions"]["RandomRange"]["anyOf"][1:]
-    else:
-        debug("Failed to patch RandomRange")
-
-    # Property "filename" is mandatory in types/SpriteSource.html
-    # but overridden to be optional in types/Sprite.html
-    # So, for the moment, we make it optional in SpriteSource
-    if "filename" in schema["definitions"].get("SpriteSource", {}).get("required", []):
-        schema["definitions"]["SpriteSource"]["required"].remove("filename")
-    else:
-        debug("Failed to patch SpriteSource")
 
     json.dump(schema, sys.stdout, indent=2)
 
