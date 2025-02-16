1f223717f3a1fba672f94b6162e8bf8bb74c0c6a
Vincent Jacques <vincent@vincent-jacques.net>
2025-02-16 10:23:28 +0100

    Homogenize how we patch the schema

--- factorio_data_raw_json_schema/patching.py
+++ factorio_data_raw_json_schema/patching.py
@@ -13,35 +13,29 @@ def patch(schema: Any) -> None:
     # Ad-hoc patches because the doc doesn't match the actual data
     # ============================================================
 
+    # Empty arrays are serialized as {} instead of []
+    # (patched in the generated schema because this must be done everywhere)
+
     # types/ItemProductPrototype.html#amount_min is documented as uint16
     # but is some sort of floating point in e.g. 'speed-module-recycling'
     if schema["definitions"].get("ItemProductPrototype", {}).get("properties", {}).get("amount", {}) == {
         "$ref": "#/definitions/uint16"
     }:
-        schema["definitions"]["ItemProductPrototype"]["properties"]["amount"] = {"$ref": "#/definitions/double"}
+        schema["definitions"]["ItemProductPrototype"]["properties"]["amount"]["$ref"] = "#/definitions/double"
     else:
         debug("Failed to patch ItemProductPrototype")
 
-    # types/TriggerEffect.html documents DamageTileTriggerEffectItem as having type="damage-tile",
-    # but DamageTileTriggerEffectItem.html documents it as having type="damage"
-
-    # types/TriggerEffect.html refers to non-documented type DamageEntityTriggerEffectItem,
-    # and types/DamageTriggerEffectItem.html is documented but used nowhere
-
-    # Empty arrays are serialized as {} instead of []
-    # (patched in the generated schema because this must be done everywhere)
-
-    # I'm fed up with documenting every single patch
-    schema["definitions"]["WorkingVisualisations"]["properties"]["working_visualisations"] = {}
-    schema["definitions"]["CharacterPrototype"]["properties"]["synced_footstep_particle_triggers"] = {}
-    remove_all_constraints("TriggerEffect")
+    # Patches to investigate and document
     remove_all_constraints("Animation4Way")
-    remove_all_constraints("Sound")
-    remove_all_constraints("Sprite4Way")
-    remove_all_constraints("WorkingSound")
-    remove_all_constraints("SpriteVariations")
-    remove_all_constraints("Sprite")
     remove_all_constraints("CharacterArmorAnimation")
+    remove_all_constraints("CraftingMachineGraphicsSet")
+    remove_all_constraints("FootstepTriggerEffectList")
+    remove_all_constraints("Sound")
+    remove_all_constraints("Sprite")
+    remove_all_constraints("Sprite4Way")
+    remove_all_constraints("SpriteVariations")
+    remove_all_constraints("TriggerEffect")
+    remove_all_constraints("WorkingSound")
 
     # Ad-hoc patches because our extraction tool is weak
     # ==================================================
